70a119033e64494e392fcd28010186df9a903ceb
Sumit Patil <sumitpatil8258@gmail.com>
2026-07-21 11:57:21 +0530

    Add files via upload

---
+++ etl.py
@@ -0,0 +1,37 @@
+from pyspark.sql import SparkSession
+
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder \
+    .appName("AQI ETL") \
+    .getOrCreate()
+
+# EXTRACT
+df = spark.read.csv(
+    "Dataset.csv",
+    header=True,
+    inferSchema=True
+)
+
+#df.show()
+
+# TRANSFORM
+df = df.dropna()
+df = df.dropDuplicates()
+df1 = df.select("NAME","HUMIDITY","TEMPRATURE_MAX")
+df_filtered = df1.filter((col("HUMIDITY") > 20) & (col("TEMPRATURE_MAX" )> 40))
+df_filtered.show()
+
+
+# LOAD
+df_filtered.write\
+.format("jdbc")\
+.option("url", "jdbc:mysql://localhost:3307/pharmacydb")\
+.option("driver", "com.mysql.cj.jdbc.Driver")\
+.option("dbtable", "AQI")\
+.option("user", "root") \
+.option("password", "sumit@123") \
+.mode("overwrite") \
+.save()
+
+spark.stop()
